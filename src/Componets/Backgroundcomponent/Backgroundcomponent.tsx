b96358b4aee5020fe285bc79aa128504d510e59b
shaikrasheed <shaikrasheed6341@gmail.com>
2025-03-11 20:15:51 +0530

    Removed nested Git repository

---
+++ src/Componets/Backgroundcomponent/Backgroundcomponent.tsx
@@ -0,0 +1,9 @@
+const Backgroundcomponent = () => {
+    return (
+        <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+            <div className="absolute top-0 z-[-2] h-screen w-screen bg-transparent bg-[radial-gradient(#ffffff33_1px,transparent_1px)] bg-[size:20px_20px]"></div>
+        </div>
+    )
+}
+
+export default Backgroundcomponent;
---
+++ src/Layout/Layout.tsx
@@ -0,0 +1,12 @@
+
+import Backgroundcomponent from '../Componets/Backgroundcomponent/Backgroundcomponent';
+import Navbar from '../Componets/Navbar/Navbar';
+function Layout() {
+  return (
+    <div><Backgroundcomponent />
+    <Navbar />
+    </div>
+  )
+}
+
+export default Layout
